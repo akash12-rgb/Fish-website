f333157ea7d7511b34d617ca02bb5941c98520a5
akash12-rgb <147169316+akash12-rgb@users.noreply.github.com>
2026-03-16 12:11:21 +0530

    Update footer links to point to correct pages

--- includes/footer.php
+++ includes/footer.php
@@ -27,8 +27,8 @@
         <ul class="footer-links">
           <li><a href="<?= APP_URL ?>/index.php#about">About Us</a></li>
           <li><a href="<?= APP_URL ?>/contact.php">Contact</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Terms of Service</a></li>
+       <li><a href="<?= APP_URL ?>/privacy-policy.php">Privacy Policy</a></li>
+<li><a href="<?= APP_URL ?>/terms-of-service.php">Terms of Service</a></li>
         </ul>
       </div>
       <div class="col-lg-4">
